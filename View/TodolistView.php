5946bff844d35b648c4763f9adfff5e58b86b8f9
Fikry Aziz Septian <azizfikry15@gmail.com>
2023-12-16 21:04:44 +0700

    create todolist view

--- View/TodolistView.php
+++ View/TodolistView.php
@@ -1,10 +1,43 @@
 <?php
 
 namespace View {
+
+  use Helper\InputHelper;
+  use Service\TodolistService;
+
   class TodolistView
   {
+    private TodolistService $todolistService;
+
+    public function __construct(TodolistService $todolistService)
+    {
+      $this->todolistService = $todolistService;
+    }
+
     function showTodolist(): void
     {
+      while (true) {
+        $this->todolistService->showTodolist();
+
+        echo "MENU" . PHP_EOL;
+        echo "1. Tambah Todo" . PHP_EOL;
+        echo "2. Hapus Todo" . PHP_EOL;
+        echo "x. Hapus Todo" . PHP_EOL;
+
+        $pilihan = InputHelper::input("Pilih");
+
+        if ($pilihan == 1) {
+          $this->addTodolist();
+        } else if ($pilihan == 2) {
+          $this->removeTodolist();
+        } elseif ($pilihan == "x") {
+          break;
+        } else {
+          echo "Pilihan tidak dimengerti" . PHP_EOL;
+        }
+      }
+
+      echo "Sampai Jumpa Lagi" . PHP_EOL;
     }
 
     function addTodolist(): void
