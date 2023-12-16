b65e9356d0f4cab41eebbe97dd2303e849076fbd
Fikry Aziz Septian <azizfikry15@gmail.com>
2023-12-16 20:46:15 +0700

    unitest add todolist

--- Test/TodolistServiceTest.php
+++ Test/TodolistServiceTest.php
@@ -4,19 +4,32 @@ require_once __DIR__ . '/../Service/TodolistService.php';
 require_once __DIR__ . '/../Repository/TodolistRepository.php';
 require_once __DIR__ . '/../Entity/Todolist.php';
 
+use Entity\Todolist;
 use Repository\TodolistRepositoryImpl;
 use Service\TodolistServiceImpl;
 
 function testShowTodolist()
 {
   $todolistRepository = new TodolistRepositoryImpl();
-  $todolistRepository->todolist[1] = "Belajar  PHP";
-  $todolistRepository->todolist[2] = "Belajar  PHP OOP";
-  $todolistRepository->todolist[3] = "Belajar  PHP Database";
+  $todolistRepository->todolist[1] = new Todolist("Belajar PHP");
+  $todolistRepository->todolist[2] = new Todolist("Belajar PHP OOP ");
+  $todolistRepository->todolist[3] = new Todolist("Belajar PHP Database");
 
   $todolistService = new TodolistServiceImpl($todolistRepository);
 
   $todolistService->showTodolist();
 }
 
-testShowTodolist();
+function testAddTodolist()
+{
+  $todolistRepository = new TodolistRepositoryImpl();
+
+  $todolistService = new TodolistServiceImpl($todolistRepository);
+  $todolistService->addTodolist("Belajar PHP");
+  $todolistService->addTodolist("Belajar PHP OOP");
+  $todolistService->addTodolist("Belajar PHP Database");
+
+  $todolistService->showTodolist();
+}
+
+testAddTodolist();
